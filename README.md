79604c538960f6026dd8a6991718eb50db9993d5
JacopoLombardi <lombajacopo@gmail.com>
2024-02-29 15:21:49 +0100

    [add] Task 1 - 2 completed

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Griglia CampoMinato
 ===
 1. Creare lo scheletro della griglia sull'HTML e CSS.
-2. Passiamo su Js dove creeremo le Cell automaticamente.
-3. 
+2. Passiamo su Js dove creeremo 100 Cell automaticamente.
+3. Numeriamo le Cell dentro il ciclo For.
 4. 
 5. 
--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 
+const containerCell = document.querySelector('._container_cell');
+const btnStart = document.querySelector('.btn_start');
+
+console.log(btnStart)
 
 
 
 
+btnStart.addEventListener('click', function(){
+
+   for( let i = 1; i <= 100; i++){
+      containerCell.innerHTML += `<div class="_cell  d-flex">${i}</div>`;
+   }
 
 
 
+})
 
 
 
+/////////// FUNCTIONS /////////////////
